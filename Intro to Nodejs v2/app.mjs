b617f2500063fdf757685ed6327ae069f4f7518f
zubairpluton <zubair.pluton@gmail.com>
2022-08-20 15:05:55 +0500

    code[read] and [write] files

--- Intro to Nodejs v2/app.mjs
+++ Intro to Nodejs v2/app.mjs
@@ -5,6 +5,26 @@
 
 // ============Module-Ecmascript-Style==============
 import { StartNodejs } from "./index.mjs";
-import {readFile} from 'fs/Promises'
+import { readFile, writeFile } from "fs/promises";
+import { write } from "fs";
+import { url } from "inspector";
+let template = await readFile(
+  new URL("template.html", import.meta.url),
+  "utf-8"
+);
+
+const data = {
+  title: "zubair",
+  para: "Started Learning Nodejs",
+};
+
+for (let [k, v] of Object.entries(data)) {
+  template = template.replace(`{${k}}`, v);
+}
+
+await writeFile(new URL("template.html", import.meta.url), template);
+
+// console.log(template.toString())
+console.log(template);
 
 StartNodejs();
